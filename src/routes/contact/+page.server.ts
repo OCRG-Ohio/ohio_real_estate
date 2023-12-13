09d94064b1878bfa7be874c48eec2af2b1d6e91d
mastmagan007 <tshanajk@gmail.com>
2023-12-13 05:59:31 +0500

    Chal Kaka, aj last day hy tera

---
+++ src/routes/contact/+page.server.ts
@@ -0,0 +1,11 @@
+import { getArticles, getPage } from '$lib/api';
+export async function load({ locals }) {
+  const currentUser = locals.user;
+
+  const page = await getPage('contact');
+
+  return {
+    currentUser,
+    page
+  };
+}
