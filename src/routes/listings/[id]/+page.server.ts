a1f2544ce5654b48bc35eb91548d6a82fea08857
mastmagan007 <tshanajk@gmail.com>
2023-12-23 01:55:08 +0500

    Revert "Updated prisma"
    This reverts commit dbe4322f67564f33d822308ad3e39a9b69f1a3cb.

--- src/routes/listings/[id]/+page.server.ts
+++ src/routes/listings/[id]/+page.server.ts
@@ -1,8 +1,8 @@
-import { error, type Actions, redirect } from '@sveltejs/kit';
+import { error, type Actions } from '@sveltejs/kit';
 import { PrismaClient } from '@prisma/client/edge';
 import type { PageServerLoad } from './$types';
 import { uploadToSupabase } from '$lib/server/uploadToSupabase'; 
-import { deleteFromSupabase} from '$lib/server/deleteFromSupabase'; 
+import { deleteFromSupabase} from '$lib/server/deleteFromSupabase '; 
 
 const prisma = new PrismaClient();
 
@@ -12,14 +12,16 @@ export const load: PageServerLoad = async ({ params }) => {
             where: {
                 id: Number(params.id),
             },
-            
+            include: {
+                gallery: true // Include related assets
+            }
         });
 
         if (!property) {
             throw error(404, "Property not found");
         }
 
-       
+        // Assuming the first asset is the featured image
        
         
         return {
@@ -35,78 +37,88 @@ export const actions: Actions = {
 	updateProperty: async ({ request, params }) => {
 	  const formData = await request.formData();
 	  const propertyId = Number(params.id);
-
 	  const propertyData = {
-		title: formData.get('title'),
-		content: formData.get('content'),
-		address: formData.get('address'),
-		city_state: formData.get('city_state'),
-		price: parseFloat(formData.get('price').toString()),
-		beds: parseInt(formData.get('beds').toString()),
-		baths: parseInt(formData.get('baths').toString()),
-		area: parseInt(formData.get('area').toString()),
-	  
-		category:formData.get('category'),
-		type:formData.get('type'),
+		  title: formData.get('title'),
+		  content: formData.get('content'),
+		  address: formData.get('address'),
+		  city_state: formData.get('city_state'),
+		  price: parseFloat(formData.get('price').toString()),
+		  beds: parseInt(formData.get('beds').toString()),
+		  baths: parseInt(formData.get('baths').toString()),
+		  area: parseInt(formData.get('area').toString()),
+		  type: selectedType,  
+		  category:formData.get('category'),
 	  };
-
-	  const featuredImageFile = formData.get('featuredImage');
-      let featuredImageUrl = null;
-      if (featuredImageFile) {
-        featuredImageUrl = await uploadToSupabase(featuredImageFile);
-      }
-	  const galleryFiles = formData.getAll('galleryImages');
-	  let galleryUrls = [];
-	  
-	  for (const file of galleryFiles) {
-		if (file instanceof File && file.size > 0) {
-		  const fileUrl = await uploadToSupabase(file);
-		  if (fileUrl) {
-			galleryUrls.push(fileUrl);
-		  } else {
-			console.error("Failed to upload file:", file.name);
-		  }
-		}
-	  }
+  
+	  try {
 		// Update property
 		await prisma.property.update({
 		  where: { id: propertyId },
-		  data: {...propertyData,
-			featuredImage:featuredImageUrl,
-		media:galleryUrls
-		}
+		  data: propertyData,
 		});
   
-		
-	throw redirect(303, `/listings/${propertyId}`)
-
-}
+		// Handle new featured image
+		const newFeaturedImage = formData.get('newFeaturedImage');
+		if (newFeaturedImage) {
+		  const featuredImageUrl = await uploadToSupabase(newFeaturedImage);
+		  // Update featured image URL in database
+		}
   
-	// deleteProperty: async ({ params }) => {
-	//   const propertyId = params.id;
+		// Handle new gallery images
+		const newGalleryImages = formData.getAll('newGalleryImages');
+		for (const file of newGalleryImages) {
+		  const imageUrl = await uploadToSupabase(file);
+		  // Insert new gallery images into database
+		}
   
-	//   try {
-	// 	// Delete all associated media from Supabase
-	// 	const media = await prisma.media.findMany({
-	// 	  where: { propertyId: propertyId }
-	// 	});
-	// 	for (const image of media) {
-	// 	  await deleteFromSupabase(image.url);
-	// 	}
+		return redirect(303, `/listings/${propertyId}`);
+	  } catch (err) {
+		console.error('Error updating property:', err);
+		throw error(500, 'Failed to update property');
+	  }
+	},
   
-	// 	// Delete property and associated media from database
-	// 	await prisma.property.delete({
-	// 	  where: { id: propertyId }
-	// 	});
+	deleteProperty: async ({ params }) => {
+	  const propertyId = params.id;
   
-	// 	return redirect(303, '/listings');
-	//   } catch (err) {
-	// 	console.error('Error deleting property:', err);
-	// 	throw error(500, 'Failed to delete property');
-	//   }
-	// }
-
-}
+	  try {
+		// Delete all associated media from Supabase
+		const media = await prisma.media.findMany({
+		  where: { propertyId: propertyId }
+		});
+		for (const image of media) {
+		  await deleteFromSupabase(image.url);
+		}
   
-
- 
+		// Delete property and associated media from database
+		await prisma.property.delete({
+		  where: { id: propertyId }
+		});
+  
+		return redirect(303, '/listings');
+	  } catch (err) {
+		console.error('Error deleting property:', err);
+		throw error(500, 'Failed to delete property');
+	  }
+	},
+  
+	deleteGalleryImage: async ({ params }) => {
+	  const imageId = params.media.asset_id;
+  
+	  try {
+		const image = await prisma.media.findUnique({
+		  where: { asset_id: imageId }
+		});
+		await deleteFromSupabase(image.url);
+  
+		await prisma.media.delete({
+		  where: { asset_id: imageId }
+		});
+  
+		return { status: 200 };
+	  } catch (err) {
+		console.error('Error deleting gallery image:', err);
+		throw error(500, 'Failed to delete gallery image');
+	  }
+	}
+  };
